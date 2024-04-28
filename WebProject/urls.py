6766d2f45d5e22b68740d46512dfd4ed6ff55125
Ayman Khaled <201125@ppu.edu.ps>
2024-04-28 22:18:30 +0300

    .

--- WebProject/urls.py
+++ WebProject/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('reports/', include('reports.urls')),
     path('users/', include('users.urls')),
+    path('courses/', include('courses.urls')),
 ]
