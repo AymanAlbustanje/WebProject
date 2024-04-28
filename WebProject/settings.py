11ae0c21fb834164b8a8732243304669da5e0e21
Ayman Khaled <201125@ppu.edu.ps>
2024-04-28 21:50:43 +0300

    .

--- WebProject/settings.py
+++ WebProject/settings.py
@@ -32,9 +32,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'analytics.apps.AnalyticsConfig',
-    # 'courses.apps.CoursesConfig',
-    # 'schedules.apps.SchedulesConfig',
-    # 'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
