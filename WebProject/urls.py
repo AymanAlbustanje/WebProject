a284500cd62fcb96e0d2444a2d1bf54b94389c87
Ayman Khaled <201125@ppu.edu.ps>
2024-04-28 22:05:15 +0300

    .

--- WebProject/urls.py
+++ WebProject/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('analytics/', include('analytics.urls')),
+    path('reports/', include('reports.urls')),
 ]
