b3338f4c783bcb7f6ce33d927a745eb4034fca2b
Ayman Khaled <201125@ppu.edu.ps>
2024-04-29 16:15:00 +0300

    .

--- courses/models.py
+++ courses/models.py
@@ -1,15 +1,14 @@
 from django.db import models
-from users.models import Student
 
 class Course(models.Model):
     course_code = models.CharField(max_length=20, unique=True)
     course_name = models.CharField(max_length=100)
     description = models.TextField()
     instructor_name = models.CharField(max_length=100)
-    schedule = models.CharField(max_length=100)
+    schedule = models.CharField(max_length=100)  # No need for related_name here
     prerequisites = models.ManyToManyField('self', blank=True)
     capacity = models.PositiveIntegerField()
 
 class Enrollment(models.Model):
-    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    student = models.ForeignKey('users.Student', on_delete=models.CASCADE)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
--- schedules/models.py
+++ schedules/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from courses.models import Course
 
-# Create your models here.
+class Schedule(models.Model):
+    days = models.CharField(max_length=100)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    room_no = models.CharField(max_length=20)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='schedules')
