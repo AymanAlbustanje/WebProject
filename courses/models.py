af6bf0734d59f8437df22582ee47fdcd73cc81c6
Ayman Khaled <201125@ppu.edu.ps>
2024-04-29 16:08:12 +0300

    .

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from users.models import Student
 
-# Create your models here.
+class Course(models.Model):
+    course_code = models.CharField(max_length=20, unique=True)
+    course_name = models.CharField(max_length=100)
+    description = models.TextField()
+    instructor_name = models.CharField(max_length=100)
+    schedule = models.CharField(max_length=100)
+    prerequisites = models.ManyToManyField('self', blank=True)
+    capacity = models.PositiveIntegerField()
+
+class Enrollment(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
