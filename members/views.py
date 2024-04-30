d751138bd9b86ace3203a44632d260b14360cfc3
Ayman Khaled <201125@ppu.edu.ps>
2024-04-30 18:11:29 +0300

    .

--- members/views.py
+++ members/views.py
@@ -3,4 +3,22 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 # Create your views here.
 def login_user(request):
-    return render(request, 'members/login.html')
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('mainpage')
+        
+        else:
+            messages.success(request,"there was an error logging in,try again")
+            return redirect('login')
+        
+    else:
+        return render(request, 'members/login.html')
+    
+def logout_user(request):
+    logout(request)
+    return redirect('mainpage')
+
